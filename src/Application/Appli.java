894409f2559a537cd60c5635085b38007b39afa5
AboAlwalid <ahmdhrere@gmail.com>
2021-10-15 16:55:40 +0200

    Merge branch 'main' of https://github.com/Hourani963/AAV_SacADos

--- src/Application/Appli.java
+++ src/Application/Appli.java
@@ -7,7 +7,7 @@ public class Appli {
     public static void main(String[] args) {
         //String chemin = "C:\\\\Users\\\\UGARIT\\\\Desktop\\\\Ahmad\\\\DUT\\\\A2 S1\\\\AAV\\\\projet\\\\items.txt";
         //String chemin = args[1];
-        String chemin = "C:\\Users\\UGARIT\\Desktop\\GIT\\AAV_SacADos\\text\\items.txt";
+        String chemin = "C:\\\\Users\\\\AboAlwalid\\\\Desktop\\\\AAV\\\\AAV_SacADos\\\\text\\\\items.txt";
         SacADos sac = new SacADos(chemin, 6);
         SacADos sac2 = new SacADos(chemin, 6);
         SacADos sac3 = new SacADos(chemin, 6);
